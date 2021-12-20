de09e13424604e462702f1d441b41484bd42ce96
danielpine <danielpine@sina.com>
2021-12-20 21:52:07 +0800

    Init

---
+++ src/main/java/danielpine/rabbitmq/delay/queue/Consumer.java
@@ -0,0 +1,12 @@
+package danielpine.rabbitmq.delay.queue;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer implements MessageListener {
+    public void onMessage(Message message) {
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/danielpine/rabbitmq/delay/queue/SpringBootRabbitmqDelayQueueApplication.java
@@ -0,0 +1,12 @@
+package danielpine.rabbitmq.delay.queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitmqDelayQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRabbitmqDelayQueueApplication.class, args);
+    }
+}
